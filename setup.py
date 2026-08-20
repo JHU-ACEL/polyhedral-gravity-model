f6c103157c96436a2d7ddbfe46c4b8841f75d498
Patrick Schwartz <pschwa24@jh.edu>
2026-08-20 17:28:19 -0400

    added cmake args so that pip install . works properly

--- setup.py
+++ setup.py
@@ -117,6 +117,8 @@ class CMakeBuild(build_ext):
         cmake_args = [
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
             f"-DPYTHON_EXECUTABLE={sys.executable}",
+            f"-DPython_EXECUTABLE={sys.executable}",
+            f"-DPython3_EXECUTABLE={sys.executable}",
         ]
         build_args = []
 
